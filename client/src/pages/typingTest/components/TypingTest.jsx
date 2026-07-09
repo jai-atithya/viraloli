ad70b6b03298980813d2f6ae7dcdca42bf07218e
Jai Atithya A <jaiatithya@gmail.com>
2026-07-09 11:31:56 +0530

    basic navbar done

--- client/src/pages/typingTest/components/TypingTest.jsx
+++ client/src/pages/typingTest/components/TypingTest.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Navbar } from '../../components/Navbar'
 
 export const TypingTest = () => {
   return (
-    <div>TypingTest</div>
+    <>
+        <div className='flex flex-col justify-center h-screen w-screen p-[1rem]'>
+            <div className='h-[10vh]'>
+                <Navbar />
+            </div>
+            <div className='flex-1'>TypingTest</div>
+        </div>
+    </>
   )
 }
