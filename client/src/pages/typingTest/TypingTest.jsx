c6a307066318b9fff4aa193c0327b26d7c83dbec
Jai Atithya A <jaiatithya@gmail.com>
2026-07-09 14:07:11 +0530

    Basic layout for practise done

---
+++ client/src/pages/typingTest/TypingTest.jsx
@@ -0,0 +1,29 @@
+import React,{useState} from 'react'
+import { Navbar } from '../components/Navbar'
+import { SecondaryNavbar } from '../components/SecondaryNavbar'
+import { Practise } from './components/Practise';
+
+export const TypingTest = () => {
+    const [current, setCurrent] = useState(0);
+    const secondaryNavItems = ["Practice", "Test"];
+    return (
+        <>
+            <div className='flex flex-col justify-center h-[100vh] w-[100vw] p-[1rem]'>
+                <div className='h-[10vh]'>
+                    <Navbar />
+                </div>
+                <div className='p-[1rem]'>
+                    <SecondaryNavbar List={secondaryNavItems} current={current} setCurrent={setCurrent} />
+                </div>
+                <div className='flex-1 w-full h-full p-[1rem]'>
+                    {current === 0 && (
+                        <Practise />
+                    )}
+                    {current === 1 && (
+                        <div>Test</div>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
