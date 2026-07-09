e2015bb5b9148ca2f2b01bff20f9496f7d8a8820
Jai Atithya A <jaiatithya@gmail.com>
2026-07-09 12:05:32 +0530

    Basic secondary navbar done

--- client/src/pages/typingTest/components/TypingTest.jsx
+++ client/src/pages/typingTest/components/TypingTest.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Navbar } from '../../components/Navbar'
+import { SecondaryNavbar } from '../../components/SecondaryNavbar'
 
 export const TypingTest = () => {
-  return (
-    <>
-        <div className='flex flex-col justify-center h-screen w-screen p-[1rem]'>
-            <div className='h-[10vh]'>
-                <Navbar />
+    const secondaryNavItems = ["Practice", "Test"];
+    return (
+        <>
+            <div className='flex flex-col justify-center h-screen w-screen p-[1rem]'>
+                <div className='h-[10vh]'>
+                    <Navbar />
+                </div>
+                <div className='flex-1 bg-slate-200 p-[1rem]'>
+                    <SecondaryNavbar List={secondaryNavItems} />
+                </div>
             </div>
-            <div className='flex-1'>TypingTest</div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
