831a8d26fb9fb5640c12f02ad349ab3748efbf10
Jai Atithya A <jaiatithya@gmail.com>
2026-07-12 16:17:50 +0530

    Offline functionality Done

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,28 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),VitePWA()],
+  plugins: [react(),tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      workbox: {
+        globPatterns: ["**/*.{jsx,js,css,html,png,svg,ico}"],
+      },
+
+      manifest: {
+        name: "Viraloli",
+        short_name: "Viraloli",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/src/assets/tamilLogo.png",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 })
