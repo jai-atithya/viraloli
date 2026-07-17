cfa516d8b2e4cd72cef76235bcafe3c810826e6c
Jai Atithya A <jaiatithya@gmail.com>
2026-07-17 09:59:25 +0530

    Server initialised (#10)

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,34 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+
+  const titles = {
+    400: "Validation Error",
+    401: "Unauthorized",
+    403: "Forbidden",
+    404: "Not Found",
+    409: "Conflict",
+    500: "Internal Server Error",
+    501: "Not Implemented",
+    503: "Service Unavailable",
+  };
+  console.log("Sending error response:", {
+    success: false,
+    data: {
+      title: titles[statusCode] || "Error",
+      message: err.message,
+      stackTrace: err.stack,
+    },
+  });
+
+  res.json({
+    success: false,
+    data: {
+      title: titles[statusCode] || "Error",
+      message: err.message,
+      stackTrace: err.stack,
+    },
+  });
+};
+
+module.exports = errorHandler;
