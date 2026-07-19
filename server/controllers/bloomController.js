6454d6f7b24d0d2b696d14233214e4c6e6be80d4
Keerthika N <nkeerthika09@gmail.com>
2026-07-19 18:50:36 +0530

    Bloom filter creation done

--- server/controllers/bloomController.js
+++ server/controllers/bloomController.js
@@ -1,8 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const bloomService = require("../services/bloomService");
 
+<<<<<<< HEAD
 // @Desc Create a bloom filter
 // @Route POST /api/bloom/init
+=======
+>>>>>>> c1aa06e (Bloom filter creation done)
 const initializeBloomFilter = asyncHandler(async (req, res) => {
     const bloom = await bloomService.getBloomFilter();
     if (bloom) {
@@ -21,7 +24,11 @@ const initializeBloomFilter = asyncHandler(async (req, res) => {
 });
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = { initializeBloomFilter };
 =======
 module.exports = { initializeBloomFilter };
 >>>>>>> f212782 (Merge conflict resolved)
+=======
+module.exports = {initializeBloomFilter};
+>>>>>>> c1aa06e (Bloom filter creation done)
