73ae22326d0bd610d593d754879c72e8c91a9e3d
Keerthika N <nkeerthika09@gmail.com>
2026-07-19 21:20:18 +0530

    Merge conflict resolved

--- server/services/userService.js
+++ server/services/userService.js
@@ -6,18 +6,27 @@ const getUserbyEmail = async (email) => {
     return await User.findOne({ email: { $eq: email } });
 };
 
+<<<<<<< HEAD
 // ===== USER DETAILS BY USERNAME =====
 const getUserbyUsername = async (username) => {
     return await User.findOne({ username: { $eq: username } });
 
+=======
+>>>>>>> f212782 (Merge conflict resolved)
 // ==== GET MY DATA ====
 const getMyData = async (userId) => {
     const user = await UserCred.findById(userId, { password: 0, __v: 0, createdAt: 0, updatedAt: 0, googleId: 0 });
     return user;
+}
+
+// ===== USER DETAILS BY USERNAME =====
+const getUserbyUsername = async (username) => {
+    return await User.findOne({ username: { $eq: username } });
 };
 
 module.exports = {
     getUserbyEmail,
     getUserbyUsername
     getMyData,
+    getUserbyUsername
 };
