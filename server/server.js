708db72b3d9a3112af9393160964ef6abea92567
Keerthika N <nkeerthika09@gmail.com>
2026-07-19 21:18:38 +0530

    Bloom filter logic created

--- server/server.js
+++ server/server.js
@@ -20,11 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 require('./config/passport');
 app.use(passport.initialize());
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 app.use("/api/auth", require('./routes/authRoutes'));
-<<<<<<< HEAD
 app.use("/api/upload", require('./routes/uploadRoutes'));
 app.use("/api/user", require('./routes/userRoutes'));
 app.use("/api/bloom", require('./routes/bloomRoutes'));
@@ -32,11 +29,7 @@ app.use("/api/characters", require('./routes/characterRoutes'));
 app.use("/api/words", require('./routes/wordRoutes'));
 app.use("/api/unit", require('./routes/unitRoutes'));
 app.use("/api/lesson", require('./routes/lessonRoutes'));
-
-=======
 app.use("/api/bloom", require('./routes/bloomRoutes'));
->>>>>>> c1aa06e (Bloom filter creation done)
-
 app.use(errorHandler);
 app.listen(PORT, () => {
     dbConnect.ConnectToMongoDB();
