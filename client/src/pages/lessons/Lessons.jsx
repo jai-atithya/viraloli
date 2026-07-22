165c2e2b5c979dbea0c2a07c5af70fb2a4b40436
Jai Atithya A <jaiatithya@gmail.com>
2026-07-23 00:35:33 +0530

    Lessons updated

--- client/src/pages/lessons/Lessons.jsx
+++ client/src/pages/lessons/Lessons.jsx
@@ -8,22 +8,31 @@ import { Lesson4 } from "./components/Lesson4";
 import { Lesson5 } from "./components/Lesson5";
 import { Lesson6 } from "./components/Lesson6";
 import { Lesson7 } from "./components/Lesson7";
-
+import { Navbar } from '../components/Navbar';
 
 export const Lessons = () => {
-  const {unitId, lessonNumber} = useParams();
+  const { unitId, lessonNumber } = useParams();
   const { language } = useLanguage();
   console.log(unitId, lessonNumber)
   return (
-    <div>
-      {lessonNumber ==1 && (<Lesson1 />)}
-      {lessonNumber ==2 && (<Lesson2 />)}
-      {lessonNumber ==3 && (<Lesson3 />)}
-      {lessonNumber ==4 && (<Lesson4 />)}
-      {lessonNumber ==5 && (<Lesson5 />)}
-      {lessonNumber ==6 && (<Lesson6 />)}
-      {lessonNumber ==7 && (<Lesson7 />)}
+    <>
 
-    </div>
+      <div className='flex flex-col h-[100vh] w-[100vw] p-[1rem] gap-[1rem] bg-white'>
+        <div className='h-[10vh]'>
+          <Navbar />
+        </div>
+        <div>
+          {lessonNumber == 1 && (<Lesson1 />)}
+          {lessonNumber == 2 && (<Lesson2 />)}
+          {lessonNumber == 3 && (<Lesson3 />)}
+          {lessonNumber == 4 && (<Lesson4 />)}
+          {lessonNumber == 5 && (<Lesson5 />)}
+          {lessonNumber == 6 && (<Lesson6 />)}
+          {lessonNumber == 7 && (<Lesson7 />)}
+
+        </div>
+      </div>
+    </>
+
   )
 }
